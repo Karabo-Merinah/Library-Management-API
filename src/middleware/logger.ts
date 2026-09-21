ee6abaa4ba91858e912642ae8f2bab1953fca824
Karabo-Merinah <skosanamerinah@gmail.com>
2026-09-21 15:56:32 +0200

    Added readme file sections for app features ,installation guide and endpoints

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,8 +1,7 @@
 import type {Request ,Response ,NextFunction} from "express"
 
 export const loggerMiddleware=(req:Request,res:Response,next:NextFunction)=>{
-    const now =new Date().toISOString()
-    console.log(`[${now}] ${req.method} ${req.url}`)
+    console.log(`${req.method} ${req.url}`)
     //without this request stucks in middle and app doesn't send any request 
     next()
 }
